4cd3688e9c78a3a45c3af4543b33f16b5639222b
Igor Sant'ana <igorba2005@gmail.com>
2019-11-16 16:51:09 -0300

    refactor: Change generic interface to string

--- pkg/queues/domain/queue.go
+++ pkg/queues/domain/queue.go
@@ -1,24 +1,42 @@
 package domain
 
+import (
+	"errors"
+	"strings"
+)
+
 type Queue struct {
-	data []interface{}
+	data []string
 }
 
-func NewQueue() *Queue {
-	return &Queue{}
+func NewQueue(elements ...string) *Queue {
+	queue := &Queue{}
+
+	if elements != nil {
+		queue.data = elements
+	}
+
+	return queue
 }
 
-func (queue *Queue) Enqueue(element interface{}) {
-	queue.data = append(queue.data, element)
+func (queue *Queue) Enqueue(element string) error {
+	trimmedElement := strings.TrimSpace(element)
+
+	if trimmedElement == "" {
+		return errors.New("Element can't be null")
+	}
+
+	queue.data = append(queue.data, trimmedElement)
+	return nil
 }
 
-func (queue *Queue) Dequeue() interface{} {
+func (queue *Queue) Dequeue() string {
 	if len(queue.data) > 0 {
 		element := queue.data[0]
 		queue.data = queue.data[1:]
 		return element
 	}
-	return nil
+	return ""
 }
 
 func (queue *Queue) Size() int {
@@ -26,5 +44,12 @@ func (queue *Queue) Size() int {
 }
 
 func (queue *Queue) Clear() {
-	queue.data = []interface{}{}
+	queue.data = []string{}
+}
+
+func (queue Queue) Elements() []string {
+	dataCopy := make([]string, len(queue.data))
+	copy(dataCopy, queue.data)
+
+	return dataCopy
 }
--- pkg/queues/domain/queue_test.go
+++ pkg/queues/domain/queue_test.go
@@ -9,16 +9,16 @@ func TestDequeueReturnsNilIfQueueIsEmpty(t *testing.T) {
 	q := NewQueue()
 	value := q.Dequeue()
 
-	assert.Nil(t, value)
+	assert.Empty(t, value)
 }
 
 func TestQueueWorksLikeFIFO(t *testing.T) {
 	q := NewQueue()
-	q.Enqueue(1)
-	q.Enqueue(2)
+	q.Enqueue("1")
+	q.Enqueue("2")
 
-	assert.Equal(t, q.Dequeue(), 1)
-	assert.Equal(t, q.Dequeue(), 2)
+	assert.Equal(t, q.Dequeue(), "1")
+	assert.Equal(t, q.Dequeue(), "2")
 }
 
 func TestQueueSize(t *testing.T) {
@@ -26,7 +26,7 @@ func TestQueueSize(t *testing.T) {
 
 	assert.Zero(t, q.Size())
 
-	q.Enqueue(1)
+	q.Enqueue("1")
 
 	assert.Equal(t, q.Size(), 1)
 }
@@ -34,9 +34,15 @@ func TestQueueSize(t *testing.T) {
 func TestQueueClear(t *testing.T) {
 	q := NewQueue()
 
-	q.Enqueue(1)
+	q.Enqueue("1")
 	assert.Equal(t, q.Size(), 1)
 
 	q.Clear()
 	assert.Zero(t, q.Size())
+}
+
+func TestElements(t *testing.T) {
+	q := NewQueue("1")
+
+	assert.Equal(t, len(q.Elements()), 1)
 }
